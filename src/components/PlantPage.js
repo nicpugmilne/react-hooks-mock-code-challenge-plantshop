e15cffd48dc279b5a59ebebd291d4f2237a5cb7d
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-13 22:27:13 -0600

    commit tree 2a757b3ed15b7427bdf85ffec303b272a48836ec

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((plantArray) => setPlants(plantArray));
+  }, []);
+
+  function addPlant(newPlant) {
+    const updatedPlantsList = [...plants, newPlant];
+    setPlants(updatedPlantsList);
+  }
+  const displayedPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search onSearch={setSearchTerm} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
